0b453861c04fd64ac6897cfb651d721a3f1bb555
aditi12Tiwari <168752770+aditi12Tiwari@users.noreply.github.com>
2024-07-31 20:17:00 +0530

    Add files via upload

---
+++ SpaceComplexityDemo.java
@@ -0,0 +1,13 @@
+class SpaceComplexityDemo{
+    public static void main(String[] args){
+        //call method add for adding two values
+     add(4,5);
+    System.out.println(add(4,5));
+    }
+
+    private static int add(int i, int j) {
+        int result = i + j;
+        return result;
+        
+    }
+}
