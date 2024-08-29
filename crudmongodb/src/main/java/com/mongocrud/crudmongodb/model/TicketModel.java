09ff62499321ab277367c461ce6c42a8d7313110
aditi12Tiwari <khusadutiwari1203@gmail.com>
2024-08-29 17:27:11 +0530

    added

---
+++ crudmongodb/src/main/java/com/mongocrud/crudmongodb/model/TicketModel.java
@@ -0,0 +1,111 @@
+package com.mongocrud.crudmongodb.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//create the dataset->database
+@Document(value = "ticketcollection")
+public class TicketModel {
+    //create fields for ticket reservation system
+    //intbookingid, train no.,
+    @Id
+    private int bookingId;
+    private String trainNo;
+    private String source;
+    private String destination;
+    private String price;
+    private String birthType;
+    private String age;
+    private String gender;
+    private String dob;
+    private String name;
+    private String mobile;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getBirthType() {
+        return birthType;
+    }
+
+    public void setBirthType(String birthType) {
+        this.birthType = birthType;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
